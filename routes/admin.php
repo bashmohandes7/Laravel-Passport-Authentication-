f9641ea6bc854745c4062c6197bceec4032d2249
bashmohandes7 <diaaemad1@gmail.com>
2021-12-26 11:14:46 +0200

    specific route for admin

---
+++ routes/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\Admin\LoginController;
+use App\Http\Controllers\Api\Admin\LogoutController;
+use App\Http\Controllers\Api\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+
+// Admin  Routes
+
+Route::middleware([
+    'api',
+])
+    ->prefix('admin')
+    ->group(function () {
+        Route::post('login',            [LoginController::class, 'login']);
+
+        Route::group(['middleware' =>        ['auth:api']], function () {
+            Route::get('/logout',        [LogoutController::class, 'logout']);
+            // User Crud
+            Route::get('/users',         [UserController::class, 'index']);
+            Route::post('/users',        [UserController::class, 'store']);
+            Route::get('/users/{id}',    [UserController::class, 'show']);
+            Route::put('/users/{id}',    [UserController::class, 'update']);
+            Route::delete('/users/{id}', [UserController::class, 'destroy']);
+        });
+    });
---
+++ routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\Api\Front\ForgotController;
+use App\Http\Controllers\Api\Front\LoginController;
+use App\Http\Controllers\Api\Front\LogoutController;
+use App\Http\Controllers\Api\Front\RegisterController;
+use App\Http\Controllers\Api\Front\ResetController;
+use App\Http\Controllers\Api\PermissionController;
+use App\Http\Controllers\Api\RoleController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(
+    'api'
+)->group(function () {
+// Unauthenticated Users
+    Route::post('register',         [RegisterController::class, 'register']);
+    Route::post('login',            [LoginController::class, 'login']);
+    Route::post('forgot-password',  [ForgotController::class, 'forgot']);
+    Route::post('reset-password',   [ResetController::class, 'reset']);
+});
+
+// Role Crud
+Route::middleware([
+    'auth:api',
+])->group(function (){
+    Route::get('logout',         [LogoutController::class,  'logout']);
+    Route::get('/roles',         [RoleController::class,    'index']);
+    Route::post('/roles',        [RoleController::class,    'store']);
+    Route::get('/roles/{id}',    [RoleController::class,    'show']);
+    Route::put('/roles/{id}',    [RoleController::class,   'update']);
+    Route::delete('/roles/{id}', [RoleController::class,   'destroy']);
+// Permission index
+    Route::get('/permissions',   [PermissionController::class, 'index']);
+});
+
