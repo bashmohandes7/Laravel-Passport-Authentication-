deefbb730090d9f343b5295601bced86dd256134
bashmohandes7 <diaaemad1@gmail.com>
2022-01-16 09:54:41 +0200

    add logout for admins

--- app/Http/Controllers/Api/Admin/LogoutController.php
+++ app/Http/Controllers/Api/Admin/LogoutController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
     /**
-     * Handling Admin Logout
+     * Handling Admin Logout ''
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function logout () {
-        auth()->user()->token()->revoke();
-        auth()->user()->token()->delete();
+        auth('admin')->user()->token()->revoke();
+        auth('admin')->user()->token()->delete();
         return $this->responseApi(__('messages.admin logout'));
     }
 }
