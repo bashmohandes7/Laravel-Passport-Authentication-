81c2c9e8c96e08e6b55d6dd3fc1907c5a8fe6e2c
bashmohandes7 <diaaemad1@gmail.com>
2022-01-16 09:56:10 +0200

    some modifications

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -3,30 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Transformers\Permissions\PermissionTransformer;
+use App\Transformers\Admin\Permissions\PermissionTransformer;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('permission:permission-list', ['only' => ['index']]);
-    }
-
     /**
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        $permissions=Permission::when($request->search ,function($q) use ($request){
-            $q->where('name', 'LIKE', "%{$request->search}%");
+        $skip = ($request->has('skip')) ? $request->skip : 0;
+        // search by permission name
+        $permissions=Permission::when($request->q ,function($q) use ($request){
+            $q->where('name', 'LIKE', "%{$request->q}%");
+         // filter by role name
         })->when($request->role ,function($q) use ($request){
             $q->whereRelation('roles', 'name', $request->role);
         });
         $count = $permissions->count();
-        $skip = ($request->has('skip')) ? $request->skip : 0;
+
         if ($request->has('skip')) {
             $permissions = $permissions->orderBy('id', 'DESC')->skip($skip)->take(10)->get();
         }else{
